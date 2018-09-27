c8d5d947e28d157bddee12ec25a34dca7590d4a4
wluyima <wyclif@openmrs.org>
2018-09-27 19:34:07 -0400

    Cleaned up the celery config file

--- python/celeryconfig.py
+++ python/celeryconfig.py
@@ -1,4 +1,3 @@
 redis_port = 6381
-redis_host = 'localhost'
-broker_url = 'redis://'+redis_host+':'+redis_port.__str__()+'/'
+broker_url = 'redis://localhost:'+redis_port.__str__()+'/'
 result_backend = 'redis'
