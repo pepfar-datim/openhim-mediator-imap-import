e57a73918879a8c66ec5976a138649f44dfdaac6
wluyima <wyclif@openmrs.org>
2018-10-16 15:14:20 -0400

    Added status code field

--- scripts/status_util.py
+++ scripts/status_util.py
@@ -4,22 +4,29 @@ Clients should call this utility script rather than the import_manager to fetch 
 
 import sys
 import json
-from constants import RESPONSE_FIELD_STATUS, RESPONSE_FIELD_RESULT
-from constants import STATUS_FAILURE
+from constants import RESPONSE_FIELD_STATUS, RESPONSE_FIELD_RESULT, RESPONSE_FIELD_STATUS_CODE
+from constants import STATUS_CODE_OK, STATUS_CODE_ERROR, STATUS_CODE_BAD_REQUEST
 from import_manager import get_import_status
 
 
 response = None
 
 try:
-    import_status = get_import_status(sys.argv[1])
+    if len(sys.argv) < 2:
+        response = {
+            RESPONSE_FIELD_STATUS_CODE: STATUS_CODE_BAD_REQUEST,
+            RESPONSE_FIELD_RESULT: "Import id required"
+        }
+    else:
+        import_status = get_import_status(sys.argv[1])
+        response = {
+            RESPONSE_FIELD_STATUS_CODE: STATUS_CODE_OK,
+            RESPONSE_FIELD_STATUS: import_status.status,
+            RESPONSE_FIELD_RESULT: str(import_status.result)
+        }
+except Exception:
     response = {
-        RESPONSE_FIELD_STATUS: import_status.status,
-        RESPONSE_FIELD_RESULT: import_status.result
-    }
-except:
-    response = {
-        RESPONSE_FIELD_STATUS: STATUS_FAILURE,
+        RESPONSE_FIELD_STATUS_CODE: STATUS_CODE_ERROR,
         RESPONSE_FIELD_RESULT: 'An error occurred'
     }
 
